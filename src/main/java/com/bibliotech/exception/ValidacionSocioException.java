35848a6b4e9165fff73809ec43a2495755c5fe17
perezmatias <maag.perez@alumno.um.edu.ar>
2026-04-27 17:43:10 -0300

    feat: crear jerarquía de excepciones y validador de datos de socio (Issue #4)

---
+++ src/main/java/com/bibliotech/exception/ValidacionSocioException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class ValidacionSocioException extends BibliotecaException {
+    public ValidacionSocioException(String mensaje) {
+        super(mensaje);
+    }
+}
